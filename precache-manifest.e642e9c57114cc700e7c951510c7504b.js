23eb9fb335c989b13b6a39dc1cadb5fe6208c202
Code Sandbox <hello@codesandbox.io>
2023-02-24 02:58:44 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e642e9c57114cc700e7c951510c7504b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052dd1c3b362c2f3a60ac47baf7a3bad",
+    "url": "https://yusefabucar.github.io/csb-pqdxws/index.html"
+  },
+  {
+    "revision": "39a0419b2f8229cb749d",
+    "url": "https://yusefabucar.github.io/csb-pqdxws/static/css/main.ffebe792.chunk.css"
+  },
+  {
+    "revision": "6f1d5d26db91786ca813",
+    "url": "https://yusefabucar.github.io/csb-pqdxws/static/js/2.4234f33e.chunk.js"
+  },
+  {
+    "revision": "39a0419b2f8229cb749d",
+    "url": "https://yusefabucar.github.io/csb-pqdxws/static/js/main.c5a49db2.chunk.js"
+  },
+  {
+    "revision": "d2c4627fb9380c33e6e2",
+    "url": "https://yusefabucar.github.io/csb-pqdxws/static/js/runtime~main.d9ab7387.js"
+  }
+]);
